dd1e66707f6868df7237a28c27b61af194523c77
joey-z <865133107@qq.com>
2019-12-16 22:47:08 +0800

    哥德巴赫猜想

---
+++ Euler46.cpp
@@ -0,0 +1,37 @@
+/*************************************************************************
+	> File Name: Euler46.cpp
+	> Author: Zoe 
+	> Mail: 
+	> Created Time: 2019年03月30日 星期六 15时45分48秒
+ ************************************************************************/
+//哥德巴赫猜想
+#include <stdio.h>
+#include <inttypes.h>
+
+#define MAX_N 10000
+
+int32_t a[MAX_N + 5] = {0};
+int32_t prime[MAX_N] = {0};
+
+int32_t main() {
+    for (int32_t i = 2; i <= MAX_N; i++) {
+        if (!prime[i]) { prime[++prime[0]] = i; a[i] = 1; }
+        for (int32_t j = 1; j <= prime[0]; j++) {
+            if (i * prime[j] > MAX_N) break;
+            prime[i * prime[j]] = 1;
+            if (i % prime[j] == 0) break;
+        }
+    }
+    for (int32_t i = 1; i <= prime[0]; i++) {
+        for (int32_t n = 1; true; n++) {
+            if (prime[i] + 2 * n * n > MAX_N) break;
+            a[prime[i] + 2 * n * n] = 1;
+        }
+    }
+    for (int32_t i = 9; i <= MAX_N; i += 2){
+        if (a[i]) continue;
+        printf("%d\n", i);
+        break;
+    }
+    return 0;
+}
